a411365ad453b78bf2919a6361192c6e25e7ba44
Nick Xidis <nxidis@acm.org>
2022-06-01 17:52:06 -0400

    Linked products & types. Added hover style to link

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -8,6 +8,7 @@ class ProductTypesController < ApplicationController
 
   # GET /product_types/1 or /product_types/1.json
   def show
+    @products = @product_type.products
   end
 
   # GET /product_types/new
